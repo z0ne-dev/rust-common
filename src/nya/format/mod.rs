39618701f04951a10e6ffabb4560e4b4a252c246
Christopher König <me@kurabloodlust.eu>
2021-08-20 20:22:21 +0200

    forward all miau formats

--- src/nya/format/mod.rs
+++ src/nya/format/mod.rs
@@ -1,6 +1,24 @@
+// forward all miau formats
+pub use miau::format::*;
+
 #[cfg(feature = "hocon")]
 pub mod hocon;
 #[cfg(feature = "hjson")]
 pub mod hjson;
 #[cfg(feature = "dhall")]
 pub mod dhall;
+
+#[cfg(feature = "hocon")]
+pub fn hocon() -> hocon::Hocon {
+    hocon::Hocon::default()
+}
+
+#[cfg(feature = "hjson")]
+pub fn hjson() -> hjson::Hjson {
+    hjson::Hjson::default()
+}
+
+#[cfg(feature = "dhall")]
+pub fn dhall() -> dhall::Dhall {
+    dhall::Dhall::default()
+}
